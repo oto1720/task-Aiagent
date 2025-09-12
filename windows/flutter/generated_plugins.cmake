4c4165988b932db9c816e4e81fe59f74266e1fee
oto1720 <kotaro17206@icloud.com>
2025-09-12 14:22:45 +0900

    fix:依存関係

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,12 +6,9 @@ list(APPEND FLUTTER_PLUGIN_LIST
   cloud_firestore
   firebase_auth
   firebase_core
-  firebase_storage
-  permission_handler_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  tflite_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
